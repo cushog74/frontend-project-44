5b7c4dc4cb7f63d2e714e6abdc89cc48accf9e57
cushog74 <k.a.anton010205@gmail.com>
2023-12-12 19:40:14 +0300

    9

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,32 +1,54 @@
-import { getRandomNumber, getRandomOperator, calculate } from '../index.js';
 
-const generateQuestion = () => {
-  const number1 = getRandomNumber();
-  const number2 = getRandomNumber();
-  const operator = getRandomOperator();
-  const question = `${number1} ${operator} ${number2}`;
-  const correctAnswer = calculate(number1, operator, number2);
-  return { question, correctAnswer };
+
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100); // Генерация случайного числа
+const generateRandomOperator = () => ['+', '-', '*'][Math.floor(Math.random() * 3)]; // Генерация случайного оператора
+
+const calculate = (number1, operator, number2) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      return NaN;
+  }
 };
 
-const startCalcGame = () => {
+const playBrainCalc = () => {
+  console.log('Welcome to the Brain Calc game!');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
   console.log('What is the result of the expression?');
 
-  for (let i = 0; i < 3; i += 1) {
-    const { question, correctAnswer } = generateQuestion();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+  for (let i = 0; i < 3; i++) {
+    const number1 = generateRandomNumber();
+    const number2 = generateRandomNumber();
+    const operator = generateRandomOperator();
 
-    if (userAnswer !== correctAnswer) {
+    const question = `${number1} ${operator} ${number2}`;
+    console.log(`Question: ${question}`);
+
+    const userAnswer = parseInt(readlineSync.question('Your answer: '), 10);
+    const correctAnswer = calculate(number1, operator, number2);
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
-    console.log('Correct!');
+console.log('Correct!');
   }
 
   console.log(`Congratulations, ${name}!`);
 };
 
-export default startCalcGame;
+export default playBrainCalc;
+
 
