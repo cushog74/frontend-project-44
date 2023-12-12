55371e3d27b7cb3ff3632b7c980920ef79f71233
cushog74 <k.a.anton010205@gmail.com>
2023-12-12 19:35:05 +0300

    7

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const maxRounds = 3;
 export const runGame = (description, generateQuestion) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`); 
   console.log(description);
 
   for (let round = 1; round <= maxRounds; round += 1) {
     const { question, correctAnswer } = generateQuestion();
-    console.log(`Question: ${question}`);
+    console.log(`Question: ${question}`); 
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`); 
+      console.log(`Let's try again, ${userName}!`); 
       return;
     }
     console.log('Correct!');
